7d424679eafe4bca136f1470d44903a2b4b61d73
Pavel.Zh <pavel.zhekanov1@yandex.ru>
2026-07-18 16:04:23 +0500

    3FixJob

--- src/components/searching.js
+++ src/components/searching.js
@@ -1,11 +1,16 @@
-import {rules, createComparison} from "../lib/compare.js";
+import {rules, createComparison, defaultRules} from "../lib/compare.js";
 
 
 export function initSearching(searchField) {
+    const compare = createComparison(
+        defaultRules.skipEmptyTargetValues,
+        rules.searchMultipleFields (searchField, ['date', 'customer', 'seller'], false)
+    );
+
     // @todo: #5.1 — настроить компаратор
 
     return (data, state, action) => {
         // @todo: #5.2 — применить компаратор
-        return data;
+        return data.filter(row => compare(row, state));
     }
 }
