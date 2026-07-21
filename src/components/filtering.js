be1f374c606f92ab46d80069708e6c079378a366
Pavel.Zh <pavel.zhekanov1@yandex.ru>
2026-07-21 19:43:26 +0500

    fixFilter2

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -32,15 +32,11 @@ export function initFiltering(elements, indexes) {
 
 
         // @todo: #4.5 — отфильтровать данные используя компаратор
-        const { totalForm, totalTo, ...rest } = state;
-        const filterState = {
-            ...rest,
-            total: [
-                totalForm === "" ? "" : Number(totalForm),
-                totalTo === "" ? "" : Number(totalTo)
-            ]
-        };
+        const compare = createComparison(defaultRules);
+        console.log(state)
+        console.log(data)
+        return data.filter(row => compare(row, state));
         
-        return data.filter(row => compare(row, filterState));
+        
     }
 }
